4b4c97bdd833a28bd59f8b533ffbdfa64337a8e4
Muhammad <muhammadrizkyputraaa@gmail.com>
2024-02-07 18:07:11 +0700

    Backend Phonebook

--- routes/users.js
+++ routes/users.js
@@ -14,82 +14,82 @@ router.get('/phonebooks', async function (req, res, next) {
   }
 });
 
-router.post('/phonebooks', async function (req, res, next) {
-  try {
-    const { name, phone } = req.body
-    const user = await User.create({ name, phone })
-    res.json(user)
-  } catch (error) {
-    res.status(500).json({ err: error.message })
-  }
-});
+// router.post('/phonebooks', async function (req, res, next) {
+//   try {
+//     const { name, phone } = req.body
+//     const user = await User.create({ name, phone })
+//     res.json(user)
+//   } catch (error) {
+//     res.status(500).json({ err: error.message })
+//   }
+// });
 
-router.put('/phonebooks/:id', async function (req, res) {
-  try {
-    const id = req.params.id
-    const { name, phone } = req.body
-    const updatepb = await User.update({ name, phone }, {
-      where: {
-        id
-      },
-      returning: true,
-      plain: true
-    });
-    res.json(updatepb[1])
-  } catch (error) {
-    res.status(500).json({ err: error.message })
-  }
-});
+// router.put('/phonebooks/:id', async function (req, res) {
+//   try {
+//     const id = req.params.id
+//     const { name, phone } = req.body
+//     const updatepb = await User.update({ name, phone }, {
+//       where: {
+//         id
+//       },
+//       returning: true,
+//       plain: true
+//     });
+//     res.json(updatepb[1])
+//   } catch (error) {
+//     res.status(500).json({ err: error.message })
+//   }
+// });
 
-router.put('/phonebooks/:id/avatar', async function (req, res) {
-  const id = req.params.id
-  let avatar
-  let uploadPath
+// router.put('/phonebooks/:id/avatar', async function (req, res) {
+//   const id = req.params.id
+//   let avatar
+//   let uploadPath
 
-  if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).send('No files were uploaded.');
-  }
+//   if (!req.files || Object.keys(req.files).length === 0) {
+//     return res.status(400).send('No files were uploaded.');
+//   }
 
-  avatar = req.files.avatar
-  let fileName = Date.now() + '_' + avatar.name
-  uploadPath = path.join(__dirname, '..', 'public', 'images', fileName);
+//   avatar = req.files.avatar
+//   let fileName = Date.now() + '_' + avatar.name
+//   uploadPath = path.join(__dirname, '..', 'public', 'images', fileName);
 
-  avatar.mv(uploadPath, async function (err) {
-    if (err)
-      return res.status(500).send(err);
+//   avatar.mv(uploadPath, async function (err) {
+//     if (err)
+//       return res.status(500).send(err);
 
-    try {
-      const profil = await User.findOne({
-        where: {
-          id
-        },
-        returning: true,
-        plain: true
-      });
-      return res.status(201).json(phonebook[1])
-    } catch (error) {
-      res.status(500).json(err)
-    }
-  })
-})
+//     try {
+//       const profil = await User.findOne({
+//         where: {
+//           id
+//         },
+//         returning: true,
+//         plain: true
+//       });
+//       return res.status(201).json(phonebook[1])
+//     } catch (error) {
+//       res.status(500).json(err)
+//     }
+//   })
+// })
 
-router.delete('/phonebooks/:id', async function (req, res) {
-  try {
-    const id = req.params.id
+// router.delete('/phonebooks/:id', async function (req, res) {
+//   try {
+//     const id = req.params.id
 
-    const updatepb = await User.destroy({
-      where: {
-        id
-      },
-      returning: true,
-      plain: true
-    });
-    res.json(updatepb[1])
-  } catch (error) {
-    res.status(500).json({ err: error.message })
-  }
-});
+//     const updatepb = await User.destroy({
+//       where: {
+//         id
+//       },
+//       returning: true,
+//       plain: true
+//     });
+//     res.json(updatepb[1])
+//   } catch (error) {
+//     res.status(500).json({ err: error.message })
+//   }
+// });
 
 
 
-module.exports = router;
+// module.exports = router;
