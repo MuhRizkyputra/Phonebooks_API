cacb4eda9e398881cf36487070df56731ed830b8
Muhammad <muhammadrizkyputraaa@gmail.com>
2024-02-07 18:11:02 +0700

    Backend Phonebook

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ const fileUpload = require('express-fileupload');
 
 var app = express();
 
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(fileUpload());
-// app.use('/', indexRouter);
-// app.use('/api', usersRouter);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload());
+app.use('/', indexRouter);
+app.use('/api', usersRouter);
 
-// module.exports = app;
+module.exports = app;
